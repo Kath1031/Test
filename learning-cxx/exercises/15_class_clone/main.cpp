01f69000b2b240cc9ed5e657af913cffb421a455
Kath1031 <2528917593@qq.com>
2025-01-05 01:48:34 +0800

    第二次提交

--- learning-cxx/exercises/15_class_clone/main.cpp
+++ learning-cxx/exercises/15_class_clone/main.cpp
@@ -7,20 +7,36 @@
 class DynFibonacci {
     size_t *cache;
     int cached;
+    int size;
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(0),size(capacity) {
+
+
+    }
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+    DynFibonacci(DynFibonacci const &x) : size(x.size), cached(x.cached) {
+        cache = new size_t[size];
+        std::copy(x.cache,x.cache + size,this->cache);
+        this->cached = x.cached;
+
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci()
+    {
+        delete[] cache;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        if(cache[1] == 0) {
+            cache[1] = 1;
+            cached = 1;
+        }
+        for (cached++; cached<=i ; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
